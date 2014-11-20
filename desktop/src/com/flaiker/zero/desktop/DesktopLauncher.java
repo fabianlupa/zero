4d81bcff5db48ca71bc8312102c8bb4e383a95dd
Flaiker <flaikerfl@gmail.com>
2014-11-20 17:15:30 +0100

    Initial commit

---
+++ desktop/src/com/flaiker/zero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.flaiker.zero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.flaiker.zero.Zero;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Zero(), config);
+    }
+}
