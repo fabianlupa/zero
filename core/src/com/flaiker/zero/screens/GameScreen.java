a911e98f7fdcb19a21f65baa0335abc032e38215
Flaiker <flaikerfl@gmail.com>
2014-11-22 16:22:57 +0100

    Added basic Box2d setup with player and camera

--- core/src/com/flaiker/zero/screens/GameScreen.java
+++ core/src/com/flaiker/zero/screens/GameScreen.java
@@ -1,13 +1,53 @@
 package com.flaiker.zero.screens;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.flaiker.zero.Zero;
 
 /**
  * Screen where the game is played on
  */
 public class GameScreen extends AbstractScreen {
+    private static final float TIME_STEP           = 1 / 300f;
+    private static final int   VELOCITY_ITERATIONS = 6;
+    private static final int   POSITION_ITERATIONS = 2;
+    private static final int   PIXEL_PER_METER     = 50;
+
+    private final World              world;
+    private final Box2DDebugRenderer debugRenderer;
+    private final OrthographicCamera box2dCamera;
+
+    private float accumulator = 0;
+
     public GameScreen(Zero zero) {
         super(zero);
+        box2dCamera = new OrthographicCamera(SCREEN_WIDTH / PIXEL_PER_METER, SCREEN_HEIGHT / PIXEL_PER_METER);
+        box2dCamera.position.set(SCREEN_WIDTH / PIXEL_PER_METER / 2f, SCREEN_HEIGHT / PIXEL_PER_METER / 2f, 0);
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer = new Box2DDebugRenderer();
+
+        // create the player
+        BodyDef bdef = new BodyDef();
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+
+        bdef.position.set(0.5f, 6);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        Body playerBody = world.createBody(bdef);
+
+        shape.setAsBox(0.5f, 0.5f);
+        fdef.shape = shape;
+        playerBody.createFixture(fdef).setUserData("player");
+    }
+
+    private void doPhysicsStep(float deltaTime) {
+        float frameTime = Math.min(deltaTime, 0.25f);
+        accumulator += frameTime;
+        while (accumulator >= TIME_STEP) {
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+            accumulator -= TIME_STEP;
+        }
     }
 
     @Override
@@ -16,7 +56,12 @@ public class GameScreen extends AbstractScreen {
     }
 
     @Override
-    public void preUIrender(float delta) {}
+    public void preUIrender(float delta) {
+        box2dCamera.update();
+        debugRenderer.render(world, box2dCamera.combined);
+
+        doPhysicsStep(delta);
+    }
 
     @Override
     protected String getName() {
