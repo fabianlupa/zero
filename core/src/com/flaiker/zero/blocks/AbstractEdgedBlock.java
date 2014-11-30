9966b42667e7a93735c70f855a12c05516f1022c
Flaiker <flaikerfl@gmail.com>
2014-11-30 19:33:17 +0100

    Added class to allow for directed textures

---
+++ core/src/com/flaiker/zero/blocks/AbstractEdgedBlock.java
@@ -0,0 +1,26 @@
+package com.flaiker.zero.blocks;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by Flaiker on 30.11.2014.
+ */
+public class AbstractEdgedBlock extends AbstractBlock {
+    public AbstractEdgedBlock(World world, String material, float xPos, float yPos, EdgeDirection edgeDirection) {
+        super(world, getAtlasPathName(material, edgeDirection), xPos, yPos);
+    }
+
+    private static String getAtlasPathName(String material, EdgeDirection edgeDirection) {
+        return material + "-" + edgeDirection.toString();
+    }
+
+    public enum EdgeDirection {
+        LEFT, TOP, RIGHT, BOTTOM, TOPLEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, INNER_TOP_LEFT, INNER_TOP_RIGHT, INNER_BOTTOM_LEFT,
+        INNER_BOTTOM_RIGHT;
+
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase();
+        }
+    }
+}
